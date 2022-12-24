0500d70fba54d9e307c5c33a681da8222548906e
Jom Dollesin <codewithjom@gmail.com>
2022-12-24 17:27:49 +0800

    feat(nvim): added Conquer of Completion.

---
+++ after/plugin/coc.rc.lua
@@ -0,0 +1,25 @@
+vim.cmd [[
+let g:coc_global_extensions = [
+  \ 'coc-clangd',
+  \ 'coc-css',
+  \ 'coc-cssmodules',
+  \ 'coc-eslint',
+  \ 'coc-html',
+  \ 'coc-htmlhint',
+  \ 'coc-java',
+  \ 'coc-json',
+  \ 'coc-lua',
+  \ 'coc-pairs',
+  \ 'coc-prettier',
+  \ 'coc-prisma',
+  \ 'coc-python',
+  \ 'coc-react-refactor',
+  \ 'coc-rome',
+  \ 'coc-sh',
+  \ 'coc-snippets',
+  \ 'coc-sql',
+  \ 'coc-tailwindcss',
+  \ 'coc-tsserver',
+  \ 'coc-webview',
+  \ ]
+]]
