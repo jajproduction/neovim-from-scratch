20e9f21dce33c252becfea6bdcd7a820d94cc538
Jom Dollesin <codewithjom@gmail.com>
2022-12-29 22:57:31 +0800

    feat(nvim): new neovim setup, colorscheme.

--- lua/craftypug/plugins.lua
+++ lua/craftypug/plugins.lua
@@ -8,6 +8,7 @@ vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'Shatur/neovim-ayu'
   use {
     'svrana/neosolarized.nvim',
     requires = { 'tjdevries/colorbuddy.nvim' }
