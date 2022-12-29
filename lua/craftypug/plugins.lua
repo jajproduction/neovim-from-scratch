eb4cb674c6ba1ba4882767a361950b3a9c8016df
Jom Dollesin <codewithjom@gmail.com>
2022-12-29 10:39:13 +0800

    feat(nvim): added new package called nvim-tree.

--- lua/craftypug/plugins.lua
+++ lua/craftypug/plugins.lua
@@ -25,6 +25,7 @@ packer.startup(function(use)
   use 'goolord/alpha-nvim'
   -- use 'lukas-reineke/indent-blankline.nvim'
   use 'dense-analysis/ale'
+  use "kyazdani42/nvim-tree.lua"
 
   use 'glepnir/lspsaga.nvim' -- LSP UIs
   use 'norcalli/snippets.nvim'
