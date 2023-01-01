64f88779003818db0b096d3b7c0732cd840bca92
Jom Dollesin <codewithjom@gmail.com>
2023-01-01 09:23:25 +0800

    fix(bufferline): new setup for bufferline.

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -4,30 +4,31 @@ if (not status) then return end
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
-    always_show_bufferline = false,
+    separator_style = 'thick',
+    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+    always_show_bufferline = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
     color_icons = true
   },
   highlights = {
     separator = {
-      fg = '#073642',
-      bg = '#002b36',
+      fg = '#a9b1d6',
+      bg = '#1a1b26',
     },
     separator_selected = {
-      fg = '#073642',
+      fg = '#a9b1d6',
     },
     background = {
-      fg = '#657b83',
-      bg = '#002b36'
+      fg = '#a9b1d6',
+      bg = '#1a1b26'
     },
     buffer_selected = {
       fg = '#fdf6e3',
       bold = true,
     },
     fill = {
-      bg = '#073642'
+      bg = '#32344a'
     }
   },
 })
