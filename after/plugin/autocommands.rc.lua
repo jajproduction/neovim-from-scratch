ac85166ddb47bdbec019f9116ce8608ef0765046
Jom Dollesin <codewithjom@gmail.com>
2023-02-03 07:17:48 +0800

    fix(nvim): set new colorscheme and enhance markdown.

--- after/plugin/autocommands.rc.lua
+++ after/plugin/autocommands.rc.lua
@@ -2,6 +2,9 @@ vim.cmd [[
 
   augroup _markdown
     autocmd!
+    autocmd vimenter *.md ZenMode
+    autocmd FileType markdown setlocal nocursorline
+    autocmd FileType markdown setlocal linebreak
     autocmd FileType markdown setlocal wrap
     autocmd FileType markdown setlocal nonumber
   augroup end
