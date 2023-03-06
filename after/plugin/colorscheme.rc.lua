c4b9ddcbd86315abfd89b0dbcd01d92608ebeab0
Jom Dollesin <codewithjom@gmail.com>
2023-03-07 06:15:42 +0800

    feat(nvim): added highlight to enable the transparency of the window.

--- after/plugin/colorscheme.rc.lua
+++ after/plugin/colorscheme.rc.lua
@@ -1,1 +1,11 @@
-vim.cmd [[ colorscheme gruvbox ]]
+vim.cmd [[
+colorscheme gruvbox
+highlight Normal guibg=none
+highlight LineNr guibg=none
+highlight Folded guibg=none
+highlight NonText guibg=none
+highlight SpecialKey guibg=none
+highlight VertSplit guibg=none
+highlight SignColumn guibg=none
+highlight EndOfBuffer guibg=none
+highlight TablineFill guibg=none ]]
