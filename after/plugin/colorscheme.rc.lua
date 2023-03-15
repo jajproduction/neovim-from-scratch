5c370cd8ac5e6da63afeab029699dcc9922a7fbe
Jom Dollesin <codewithjom@gmail.com>
2023-03-15 19:16:46 +0800

    fix(nvim): bringing back the old colorscheme.

--- after/plugin/colorscheme.rc.lua
+++ after/plugin/colorscheme.rc.lua
@@ -1,11 +1,12 @@
 vim.cmd [[
-colorscheme tokyonight-night
-highlight Normal guibg=none
-highlight LineNr guibg=none
-highlight Folded guibg=none
-highlight NonText guibg=none
-highlight SpecialKey guibg=none
-highlight VertSplit guibg=none
-highlight SignColumn guibg=none
-highlight EndOfBuffer guibg=none
-highlight TablineFill guibg=none ]]
+" colorscheme tokyonight-night
+" highlight Normal guibg=none
+" highlight LineNr guibg=none
+" highlight Folded guibg=none
+" highlight NonText guibg=none
+" highlight SpecialKey guibg=none
+" highlight VertSplit guibg=none
+" highlight SignColumn guibg=none
+" highlight EndOfBuffer guibg=none
+" highlight TablineFill guibg=none
+]]
