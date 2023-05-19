d9aeee15c19c3ba0cf06a2ebf08176747c4390a2
Jom Dollesin <codewithjom@gmail.com>
2023-05-20 07:34:19 +0800

    feat: added autocommands when using markdown.

---
+++ after/plugin/autocommands.rc.lua
@@ -0,0 +1,10 @@
+vim.cmd [[
+
+  augroup _markdown
+    autocmd!
+    autocmd vimenter *.md ZenMode
+    autocmd FileType markdown setlocal linebreak
+    autocmd FileType markdown setlocal wrap
+  augroup end
+
+]]
