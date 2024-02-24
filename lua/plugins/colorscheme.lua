6ec26a7b12cd94c5bde2e8d484eafc7b63d57644
Jom Dollesin <codewithjom@gmail.com>
2024-02-24 20:56:30 +0800

    feat: new neovim setup.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,23 @@
 return {
+	-- {
+	-- 	"craftzdog/solarized-osaka.nvim",
+	-- 	lazy = true,
+	-- 	priority = 1000,
+	-- 	opts = function()
+	-- 		return {
+	-- 			transparent = true,
+	-- 		}
+	-- 	end,
+	-- },
+
+	-- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	-- { "sainnhe/gruvbox-material", priority = 1000 },
 	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
+		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
+		config = true,
 		opts = function()
-			return {
-				transparent = true,
-			}
+			return { transparent_mode = true }
 		end,
 	},
-	-- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
