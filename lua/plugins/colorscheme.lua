ee9d6ae880ac0c137c5c1d50b5250cc237dcbe77
Jom Dollesin <codewithjom@gmail.com>
2024-03-17 22:39:30 +0800

    fix: changing the colorscheme to onedark.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,17 @@
 return {
+	-- {
+	-- 	"craftzdog/solarized-osaka.nvim",
+	-- 	lazy = true,
+	-- 	priority = 1000,
+	-- 	opts = function()
+	-- 		return {
+	-- 			transparent = true,
+	-- 		}
+	-- 	end,
+	-- },
 	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
+		"olimorris/onedarkpro.nvim",
 		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
-		end,
 	},
 	-- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	-- { "sainnhe/gruvbox-material", priority = 1000 },
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -21,13 +21,13 @@ return {
 				hsl_color = {
 					pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
 					group = function(_, match)
-						local utils = require("solarized-osaka.hsl")
+						-- local utils = require("solarized-osaka.hsl")
 						--- @type string, string, string
 						local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%?%)")
 						--- @type number?, number?, number?
 						local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
 						--- @type string
-						local hex_color = utils.hslToHex(h, s, l)
+						-- local hex_color = utils.hslToHex(h, s, l)
 						return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
 					end,
 				},
