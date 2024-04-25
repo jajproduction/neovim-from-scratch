3962fb7a154e39fd84d2fbeaf0c911be4b2f4493
Jaj Dollesin <codewithjom@gmail.com>
2024-04-25 13:45:22 +0800

    feat: changing the colorscheme to catppuccin.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
 return {
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
-		end,
-	},
+	-- {
+	-- 	"craftzdog/solarized-osaka.nvim",
+	-- 	lazy = true,
+	-- 	priority = 1000,
+	-- 	opts = function()
+	-- 		return {
+	-- 			transparent = true,
+	-- 		}
+	-- 	end,
+	-- },
 	-- {
 	-- 	"olimorris/onedarkpro.nvim",
 	-- 	priority = 1000,
 	-- },
-	-- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	-- {
 	-- 	"sainnhe/gruvbox-material",
 	-- 	name = "gruvbox-material",
