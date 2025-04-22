f16064abedbce720de8fa0a6c3b49312aeb1e373
Jaj <codewithjom@gmail.com>
2025-04-22 10:37:30 +0800

    feat(ui): changing the colorscheme to darkplus.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
 			opts = {
-				colorscheme = "tokyonight-night",
+				colorscheme = "darkplus",
 				news = {
 					lazyvim = true,
 					neovim = true,
