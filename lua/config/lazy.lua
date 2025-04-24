a698ebd72d3fdd1144f9fc052e15f19ef8208318
Jaj <codewithjom@gmail.com>
2025-04-24 18:40:09 +0800

    feat(ui): changing the colorscheme to solarized osaka.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
 			opts = {
-				colorscheme = "darkplus",
+				colorscheme = "solarized-osaka",
 				news = {
 					lazyvim = true,
 					neovim = true,
