a0adcbed3e3f71bc686b0702a851aab5ef7cf896
Jaj <codewithjom@gmail.com>
2025-05-08 06:49:46 +0800

    resolve(config): changing the colorscheme to onedark_dark.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
 			opts = {
-				colorscheme = "catppuccin-mocha",
+				colorscheme = "onedark_dark",
 				news = {
 					lazyvim = true,
 					neovim = true,
