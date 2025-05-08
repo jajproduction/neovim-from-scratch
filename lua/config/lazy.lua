964db84cb5a84028a2e4ad67ffac94b98ec5a4d0
Jaj <codewithjom@gmail.com>
2025-05-09 06:50:57 +0800

    feat(ui): new colorscheme added and applied.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
 			opts = {
-				colorscheme = "onedark_dark",
+				colorscheme = "kanso-zen",
 				news = {
 					lazyvim = true,
 					neovim = true,
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -46,4 +46,9 @@ return {
 	{ "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
 	{ "thesimonho/kanagawa-paper.nvim", lazy = false, priority = 1000, opts = {} },
 	{ "rebelot/kanagawa.nvim", lazy = false, priority = 1000, opts = {} },
+	{
+		"webhooked/kanso.nvim",
+		lazy = false,
+		priority = 1000,
+	},
 }
