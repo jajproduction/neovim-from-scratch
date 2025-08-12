cf2132dbc5fcc33d6f7fdd15153553efc0b1f951
Jaj <codewithjom@gmail.com>
2025-08-12 17:24:55 +0800

    chore(config) changing the theme and the header.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
 			opts = {
-				colorscheme = "kanagawa-dragon",
+				colorscheme = "catppuccin-mocha",
 				news = {
 					lazyvim = true,
 					neovim = true,
