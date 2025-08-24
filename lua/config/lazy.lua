79808b94c25fefb0cd5aa8bcb3084d02b2bc681e
Jaj <codewithjom@gmail.com>
2025-08-24 09:59:47 +0800

    chore(ui): changing the colorscheme.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
 			opts = {
-				colorscheme = "catppuccin-mocha",
+				colorscheme = "kanso-zen",
 				news = {
 					lazyvim = true,
 					neovim = true,
